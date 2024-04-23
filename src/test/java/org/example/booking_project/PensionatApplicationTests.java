eebd985e15694f74bc2ecfd12e661eac15f243e3
Martin Sax <h.martin.sax@gmail.com>
2024-04-23 22:57:18 +0200

    added controller assert-tests

--- src/test/java/org/example/booking_project/PensionatApplicationTests.java
+++ src/test/java/org/example/booking_project/PensionatApplicationTests.java
@@ -1,13 +1,29 @@
 package org.example.booking_project;
 
+import org.example.booking_project.controllers.BookingController;
+import org.example.booking_project.controllers.CustomerController;
+import org.example.booking_project.controllers.RoomController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BookingProjectApplicationTests {
 
+    @Autowired
+    private BookingController bookingController;
+    @Autowired
+    private CustomerController customerController;
+    @Autowired
+    private RoomController roomController;
+
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        assertThat(bookingController).isNotNull();
+        assertThat(customerController).isNotNull();
+        assertThat(roomController).isNotNull();
     }
 
 }
