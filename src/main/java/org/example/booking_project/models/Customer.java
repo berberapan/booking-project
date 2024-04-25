096604afb9fb7d771111d3a41b1aecaa71ebbb6d
Martin Sax <h.martin.sax@gmail.com>
2024-04-25 12:03:55 +0200

    room-roomdto transformations added

--- src/main/java/org/example/booking_project/models/Customer.java
+++ src/main/java/org/example/booking_project/models/Customer.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Customer {
 
     @Id
