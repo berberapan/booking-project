718f4fcecf61c2c1751be9e70bd6f4e6c8a354de
Malin Patriksson <malinpatriksson@hotmail.com>
2024-04-25 21:50:46 +0200

    added service for customer

--- src/main/java/org/example/booking_project/service/CustomerService.java
+++ src/main/java/org/example/booking_project/service/CustomerService.java
@@ -3,9 +3,22 @@ package org.example.booking_project.service;
 import org.example.booking_project.Dtos.CustomerDTO;
 import org.example.booking_project.models.Customer;
 
+import java.util.List;
+
 public interface CustomerService {
 
     public CustomerDTO customerToCustomerDTO(Customer c);
 
     public Customer customerDTOToCustomer(CustomerDTO c);
+
+   public List<CustomerDTO> getAllCustomers();
+
+    public CustomerDTO createCustomer(CustomerDTO customerDTO);
+
+    public CustomerDTO getCustomerByEmail(String email);
+
+    public CustomerDTO updateCustomer(Long id, CustomerDTO customerDTO);
+
+    public void deleteCustomer(Long id);
+
 }
