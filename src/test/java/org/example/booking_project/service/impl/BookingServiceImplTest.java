7ef27ca61ad7125f59e7b643e2da40f79348f131
Martin Sax <h.martin.sax@gmail.com>
2024-04-28 23:22:46 +0200

    test of booking nr generator added.

--- src/test/java/org/example/booking_project/service/impl/BookingServiceImplTest.java
+++ src/test/java/org/example/booking_project/service/impl/BookingServiceImplTest.java
@@ -7,20 +7,31 @@ import org.example.booking_project.models.Booking;
 import org.example.booking_project.models.Customer;
 import org.example.booking_project.models.Room;
 import org.example.booking_project.models.RoomType;
+import org.example.booking_project.repos.BookingRepo;
 import org.example.booking_project.service.BookingService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class BookingServiceImplTest {
-    private BookingServiceImpl bookingService = new BookingServiceImpl();
-    private Customer testcustomer = new Customer((long) 123, "CN001", "Kalle",
+
+    @Mock
+    private BookingRepo bookingRepo;
+
+    @InjectMocks
+    private BookingServiceImpl bookingService = new BookingServiceImpl(bookingRepo);
+
+    private Customer testcustomer = new Customer((long) 123, "CN101", "Kalle",
             "012-345678", "abc@abcdef.se");
-    private CustomerDTO testcustomerDTO = new CustomerDTO((long) 123, "CN001", "Kalle",
+    private CustomerDTO testcustomerDTO = new CustomerDTO((long) 123, "CN101", "Kalle",
             "012-345678", "abc@abcdef.se");
     private Room testroom = new Room((long) 321, 101, RoomType.DOUBLE, 3, 500);
     private RoomDTO testroomDTO = new RoomDTO((long) 321, 101, 3, 500, RoomType.DOUBLE);
@@ -28,10 +39,10 @@ class BookingServiceImplTest {
     private LocalDate checkOut1 = LocalDate.parse("2024-10-02");
     private LocalDate checkOut2 = LocalDate.parse("2024-10-08");
 
-    private Booking testbooking1 = new Booking((long) 213, "BN001", testcustomer, testroom, 2, checkIn, checkOut1);
-    private Booking testbooking2 = new Booking((long) 213, "BN001", testcustomer, testroom, 2, checkIn, checkOut2);
+    private Booking testbooking1 = new Booking((long) 213, "BN101", testcustomer, testroom, 2, checkIn, checkOut1);
+    private Booking testbooking2 = new Booking((long) 213, "BN101", testcustomer, testroom, 2, checkIn, checkOut2);
 
-    private BookingDTO actualBookingDTO = new BookingDTO((long) 213, "BN001", testcustomerDTO, testroomDTO, 2, checkIn, checkOut1);
+    private BookingDTO actualBookingDTO = new BookingDTO((long) 213, "BN101", testcustomerDTO, testroomDTO, 2, checkIn, checkOut1);
 
     private BookingDTO testbdto1 = bookingService.bookingToBookingDTO(testbooking1);
     private BookingDTO testbdto2 = bookingService.bookingToBookingDTO(testbooking2);
@@ -54,5 +65,13 @@ class BookingServiceImplTest {
     @Test
     void bookingDTOToBooking() {
         assertEquals(testbooking1, bookingService.bookingDTOToBooking(actualBookingDTO,testcustomer,testroom));
+    }
+
+    @Test
+    void generateBookingNr() {
+        when(bookingRepo.findAll()).thenReturn(Arrays.asList(testbooking1));
+        BookingServiceImpl service2 = new BookingServiceImpl(bookingRepo);
+        String testBookingNr = service2.generateBookingNr();
+        assertEquals("BN102", testBookingNr);
     }
 }
