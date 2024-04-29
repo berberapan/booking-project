9fc4f73f81667f1deec33e5a51d1609e3ac3a181
Martin Sax <h.martin.sax@gmail.com>
2024-04-29 16:56:20 +0200

    startpage links to templates added

--- src/main/java/org/example/booking_project/booking_project.java
+++ src/main/java/org/example/booking_project/booking_project.java
@@ -23,6 +23,23 @@ public class booking_project {
     }
     /*
     @Bean
+    public CommandLineRunner saveRooms(RoomRepo repo) {
+        return (args) -> {
+            repo.save(new Room(null, 101, RoomType.SINGLE, 1, 600));
+            repo.save(new Room(null, 102, RoomType.SINGLE, 1, 600));
+            repo.save(new Room(null, 103, RoomType.SINGLE, 1, 400));
+            repo.save(new Room(null, 104, RoomType.SINGLE, 1, 400));
+            repo.save(new Room(null, 201, RoomType.DOUBLE, 3, 1000));
+            repo.save(new Room(null, 202, RoomType.DOUBLE, 3, 1000));
+            repo.save(new Room(null, 203, RoomType.DOUBLE, 4, 1200));
+            repo.save(new Room(null, 301, RoomType.DOUBLE, 3, 1000));
+            repo.save(new Room(null, 302, RoomType.DOUBLE, 4, 1200));
+            repo.save(new Room(null, 303, RoomType.DOUBLE, 4, 1200));
+        };
+    }
+    */
+    /*
+    @Bean
     public CommandLineRunner saveBooking(BookingRepo repo, CustomerRepo customerRepo, RoomRepo roomRepo){
         return (args) -> {
             Customer customer1 = customerRepo.findById(1L).orElse(null);
