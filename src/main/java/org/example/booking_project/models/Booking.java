f9bee014358a78b732856d0fd99391efdd83d515
Martin Sax <h.martin.sax@gmail.com>
2024-04-30 18:52:37 +0200

    booking validation added

--- src/main/java/org/example/booking_project/models/Booking.java
+++ src/main/java/org/example/booking_project/models/Booking.java
@@ -1,8 +1,7 @@
 package org.example.booking_project.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -33,12 +32,23 @@ public class Booking {
     @JoinColumn
     private Customer customer;
 
+    @NotEmpty(message = "Rum är obligatoriskt")
     @ManyToOne
     @JoinColumn
     private Room room;
 
+    @NotEmpty(message = "Bokade sängar är obligatoriskt")
+    @Min(value = 1, message = "Bokningen måste ha minst 1 bokad säng")
+    @Max(value = 4, message = "Bokningen får inte ha fler än 4 bokade sängar")
     private int bookedBeds;
+
+    @NotEmpty(message = "Incheckningsdatum är obligatoriskt")
+    @FutureOrPresent (message = "Incheckningsdatum måste vara dagens datum eller ett kommande datum")
     private LocalDate checkInDate;
+
+
+    @NotEmpty(message = "Utcheckningsdatum är obligatoriskt")
+    @Future (message = "Utcheckningsdatum måste vara ett kommande datum")
     private LocalDate checkOutDate;
 
 }
