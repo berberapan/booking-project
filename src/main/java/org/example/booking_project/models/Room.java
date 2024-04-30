2e5f2b7846e1f5e9dafd20d69a4a6f8563b94f5c
Martin Sax <h.martin.sax@gmail.com>
2024-04-30 13:55:02 +0200

    room validation added

--- src/main/java/org/example/booking_project/models/Room.java
+++ src/main/java/org/example/booking_project/models/Room.java
@@ -1,6 +1,9 @@
 package org.example.booking_project.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,11 +19,20 @@ public class Room {
     @GeneratedValue
     private Long id;
 
+    @NotEmpty(message = "Rummet måste ha ett rumsnummer")
     @Column(unique = true)
     private int roomNumber;
 
+    @NotEmpty(message = "Ange RoomTyp:singel eller dubbel")
     private RoomType roomType;
+
+    @NotEmpty(message = "Ange 1 - 4 sängar")
+    @Min(value = 1, message = "Rummet måste ha minst 1 säng")
+    @Max(value = 4, message = "Rummet får inte ha fler än 4 sängar")
     private int maxBeds;
+
+    @NotEmpty(message = "Rummet måste ha pris/natt")
+    @Min(value = 0, message = "Rummet får inte ha ett negativt pris/natt")
     private int pricePerNight;
 
 }
