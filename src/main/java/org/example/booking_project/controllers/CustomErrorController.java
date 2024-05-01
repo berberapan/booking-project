c616830210476f7694f103aa2cf9e79aa6948037
Martin Sax <h.martin.sax@gmail.com>
2024-05-02 00:09:25 +0200

    added custom error handling and templates

--- src/main/java/org/example/booking_project/controllers/CustomErrorController.java
+++ src/main/java/org/example/booking_project/controllers/CustomErrorController.java
@@ -5,15 +5,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    public String controlError(HttpServletRequest request) {
+    public String controlError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
+        Object message = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
+        if (message != null){
+            String mess = message.toString();
+            model.addAttribute("message", mess);
+        }
         if (status != null) {
             int statusCode = Integer.parseInt(status.toString());
 
