6487f04e78f04e6738f9ccb672c7210c58a940a1
Martin Sax <h.martin.sax@gmail.com>
2024-05-02 01:29:17 +0200

    added custom error handling and templates

--- src/main/java/org/example/booking_project/controllers/CustomErrorController.java
+++ src/main/java/org/example/booking_project/controllers/CustomErrorController.java
@@ -2,11 +2,14 @@ package org.example.booking_project.controllers;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class CustomErrorController implements ErrorController {
@@ -15,20 +18,27 @@ public class CustomErrorController implements ErrorController {
     public String controlError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         Object message = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
-        if (message != null){
+        /*
+        if (ex instanceof ConstraintViolationException) {
+            ConstraintViolationException constraintViolationException = (ConstraintViolationException) ex;
+            String error = constraintViolationException.getMessage();
+            model.addAttribute("error", error);
+            return "errorCVE";
+        }*/
+        if (message != null) {
             String mess = message.toString();
             model.addAttribute("message", mess);
         }
         if (status != null) {
             int statusCode = Integer.parseInt(status.toString());
 
-            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
                 return "error-404";
-            }
-            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
                 return "error-500";
             }
         }
         return "error";
     }
+
 }
