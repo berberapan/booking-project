c48afca71643d81c5dd7f4085f6c99406afae097
Maria König <maria.konig@yh.nackademin.se>
2024-05-09 19:40:44 +0200

    Adds Search bar with mock data

--- src/main/java/org/example/booking_project/controllers/ContractCustomerController.java
+++ src/main/java/org/example/booking_project/controllers/ContractCustomerController.java
@@ -1,9 +1,14 @@
 package org.example.booking_project.controllers;
 
+import org.example.booking_project.models.Customer;
 import org.example.booking_project.service.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class ContractCustomerController {
@@ -14,11 +19,20 @@ public class ContractCustomerController {
         this.customerService = customerService;
     }
 
-    @GetMapping("contract-customer/search")
+    @GetMapping("contract-customer")
     public String showContractCustomers(Model model) {
 
         //Mock data! Byt till lista av contract customers DTO i modellen
         model.addAttribute("customers",customerService.getAllCustomers());
+        return "contractCustomerTable";
+    }
+
+    @PostMapping("contract-customer/search")
+    public String searchInContractCustomer(@RequestParam String search, Model model){
+
+        //Mock data! Filtrerad lista ska in i modellen
+        List<Customer> list = List.of();
+        model.addAttribute("customers", list);
         return "contractCustomerTable";
     }
 }
