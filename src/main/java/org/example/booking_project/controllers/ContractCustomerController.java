b2455f932caf35519a40e0c9ff83c9fa4378ab67
Maria König <maria.konig@yh.nackademin.se>
2024-05-09 17:40:38 +0200

    Adds Mock data and no-content-state for table

--- src/main/java/org/example/booking_project/controllers/ContractCustomerController.java
+++ src/main/java/org/example/booking_project/controllers/ContractCustomerController.java
@@ -1,15 +1,24 @@
 package org.example.booking_project.controllers;
 
+import org.example.booking_project.service.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ContractCustomerController {
+    //Mock data! Byt till Contract customer service
+    private final CustomerService customerService;
+
+    public ContractCustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 
     @GetMapping("contract-customer/search")
     public String showContractCustomers(Model model) {
-  //Lägg till lista av contract customers DTO i modellen
+
+        //Mock data! Byt till lista av contract customers DTO i modellen
+        model.addAttribute("customers",customerService.getAllCustomers());
         return "contractCustomerTable";
     }
 }
