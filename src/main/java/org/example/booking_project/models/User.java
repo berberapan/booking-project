42d49a8d5359aee517cd84803a3db0cfcfe44548
Josef <josef@boukdir.se>
2024-05-23 02:05:11 +0200

    entities added

---
+++ src/main/java/org/example/booking_project/models/User.java
@@ -0,0 +1,28 @@
+package org.example.booking_project.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles;
+}
