283bb3ebcf987dd4f734fb588beb175ff7c1ba7d
Maria König <maria.konig@yh.nackademin.se>
2024-05-27 21:32:18 +0200

    Adds feature save/find user

--- src/main/java/org/example/booking_project/controllers/UserController.java
+++ src/main/java/org/example/booking_project/controllers/UserController.java
@@ -1,18 +1,23 @@
 package org.example.booking_project.controllers;
 
+import lombok.AllArgsConstructor;
+import org.example.booking_project.Dtos.CustomerDTO;
 import org.example.booking_project.Dtos.UserDTO;
 import org.example.booking_project.repos.UserRepo;
-import org.modelmapper.ModelMapper;
+import org.example.booking_project.service.impl.UserDetailsServiceImpl;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@AllArgsConstructor
 public class UserController {
     UserRepo userRepo;
+    UserDetailsServiceImpl userService;
 
     @GetMapping("/login")
     public String login() {
@@ -20,15 +25,39 @@ public class UserController {
     }
 
     @GetMapping("/user")
-    @PreAuthorize("hasAuthority=('admin')")
-    public String showSearchPage(){
-        return "userAccounts.html";
+    @PreAuthorize("hasAuthority('admin')")
+    public String showSearchPage() {
+        return "userAccount.html";
     }
 
     @PostMapping("/user/search")
-    @PreAuthorize("hasAuthority=('admin')")
-    public String searchUser(@RequestParam String search, Model model){
-        model.addAttribute("user", new ModelMapper().map(userRepo.getUserByUsername(search), UserDTO.class) );
-        return "userAccounts.html";
+    @PreAuthorize("hasAuthority('admin')")
+    public String searchUser(@RequestParam String search, Model model) {
+
+        UserDTO user = userService.userToUserDTO(userRepo.getUserByUsername(search));
+        model.addAttribute("user", user);
+        return "userAccount.html";
     }
+
+    @PostMapping("/user/update")
+    @PreAuthorize("hasAuthority('admin')")
+    public String updateUser(@ModelAttribute CustomerDTO customerDTO, Model model) {
+
+        return "";
+    }
+    @GetMapping("/user/create")
+    public String redirectToCreateUser(){
+        return "createUser.html";
+    }
+
+    @PostMapping("/user/create")
+    @PreAuthorize("hasAuthority('admin')")
+    public String createUser(@ModelAttribute UserDTO userDTO, Model model) {
+        System.out.println(userService.userDTOToUser(userDTO));
+        userRepo.save(userService.userDTOToUser(userDTO));
+        model.addAttribute("message", "Ny användare skapad");
+        model.addAttribute("user", userDTO);
+        return "createUser.html";
+    }
+
 }
--- src/main/java/org/example/booking_project/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/org/example/booking_project/service/impl/UserDetailsServiceImpl.java
@@ -1,7 +1,10 @@
 package org.example.booking_project.service.impl;
 
+import org.example.booking_project.Dtos.UserDTO;
 import org.example.booking_project.models.ConcreteUserDetails;
+import org.example.booking_project.models.Role;
 import org.example.booking_project.models.User;
+import org.example.booking_project.repos.RoleRepo;
 import org.example.booking_project.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,11 +12,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private RoleRepo roleRepo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -25,4 +34,22 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
         return new ConcreteUserDetails(user);
     }
+
+    public UserDTO userToUserDTO(User user) {
+        return UserDTO.builder().id(user.getId()).username(user.getUsername())
+                .admin(user.getRoles().stream().anyMatch(role -> Objects.equals(role.getName(), "admin")))
+                .receptionist(user.getRoles().stream().anyMatch(role -> Objects.equals(role.getName(), "receptionist")))
+                .build();
+    }
+
+    public User userDTOToUser(UserDTO userDTO) {
+        ArrayList<Role> roles = new ArrayList<>();
+
+        if (userDTO.getAdmin()) roles.add(roleRepo.findByName("admin"));
+        if (userDTO.getReceptionist()) roles.add(roleRepo.findByName("receptionist"));
+
+        return User.builder().username(userDTO.getUsername()).password(userDTO.getPassword()).roles(roles).build();
+    }
+
+
 }
