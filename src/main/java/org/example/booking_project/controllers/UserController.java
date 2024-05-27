91230c9ff1f0f0ae7d655e9fd4e761d3df99a14c
Maria König <maria.konig@yh.nackademin.se>
2024-05-27 14:34:05 +0200

    ???

--- src/main/java/org/example/booking_project/controllers/UserController.java
+++ src/main/java/org/example/booking_project/controllers/UserController.java
@@ -1,13 +1,34 @@
 package org.example.booking_project.controllers;
 
+import org.example.booking_project.Dtos.UserDTO;
+import org.example.booking_project.repos.UserRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
+    UserRepo userRepo;
 
     @GetMapping("/login")
     public String login() {
         return "login.html";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAuthority=('admin')")
+    public String showSearchPage(){
+        return "userAccounts.html";
+    }
+
+    @PostMapping("/user/search")
+    @PreAuthorize("hasAuthority=('admin')")
+    public String searchUser(@RequestParam String search, Model model){
+        model.addAttribute("user", new ModelMapper().map(userRepo.getUserByUsername(search), UserDTO.class) );
+        return "userAccounts.html";
     }
 }
