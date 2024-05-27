0ee647f8d5bc98152d5271f46bdfe97348816f5f
Martin Sax <h.martin.sax@gmail.com>
2024-05-27 08:40:43 +0200

    setup for roomserviceimpl

--- src/test/java/org/example/booking_project/service/impl/RoomServiceImplTests.java
+++ src/test/java/org/example/booking_project/service/impl/RoomServiceImplTests.java
@@ -1,15 +1,29 @@
 package org.example.booking_project.service.impl;
 
+import org.example.booking_project.Dtos.RoomDTO;
+import org.example.booking_project.models.Room;
+import org.example.booking_project.models.RoomType;
+import org.example.booking_project.repos.RoomRepo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class RoomServiceImplTests {
+    @Mock
+    RoomRepo roomRepo;
+    Room testroom;
+    Room testroom2;
+    RoomDTO testroomDTO;
 
+    private RoomServiceImpl roomService;
     @BeforeEach
     void setup(){
-
+        roomService = new RoomServiceImpl(roomRepo);
+        testroom = new Room((long) 321, 101, RoomType.DOUBLE, 3, 500);
+        testroom2 = new Room((long) 322, 102, RoomType.SINGLE, 1, 200);
+        testroomDTO = new RoomDTO((long) 321, 101, 3, 500, RoomType.DOUBLE);
     }
 
     @Test
